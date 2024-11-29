5e7c4bad988e5d62a9ace4dc2a4f5c6f6184f31f
pedropark99 <pedropark99@gmail.com>
2024-11-28 22:14:02 -0300

    Add cargo

--- RESOURCES.md
+++
@@ -1,5 +0,0 @@
-# Useful resources for learning
-
-## To understand image processing
-
-- https://ninedegreesbelow.com/files/dcraw-c-code-annotated-code.html
--- include/filesystem.hpp
+++
@@ -1,8 +0,0 @@
-#include <string>
-#include <filesystem>
-
-
-std::filesystem::path as_path(std::string str_path);
-std::string file_name(std::filesystem::path path);
-
-
